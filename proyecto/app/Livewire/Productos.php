1065a983865376840c1fe1c8bb5a3872162d43a7
HectorGuerra004 <hectorsantiagoguerra004@gmail.com>
2025-02-25 22:08:16 -0400

    crud de productos

--- proyecto/app/Livewire/Productos.php
+++ proyecto/app/Livewire/Productos.php
@@ -7,11 +7,77 @@ use App\Models\Producto;
 
 class Productos extends Component
 {
-    public $productos;
+    public $productos, $nombre, $descripcion, $precio, $stock, $imagen, $categoria, $estado, $id_producto;
+    public $modal = false;
     public function render()
     {
         $this->productos = Producto::all();
         return view('livewire.productos')
             ->layout('layouts.app'); // Especifica el layout aquí
+    }
+
+    public function crear()
+    {
+        $this->limpiarCampos();
+        $this->abrirModal();
+    }
+
+    public function abrirModal()
+    {
+        $this->modal = true;
+    }
+
+    public function cerrarModal()
+    {
+        $this->modal = false;
+    }
+
+    public function limpiarCampos()
+    {
+        $this->nombre = '';
+        $this->descripcion = '';
+        $this->precio = '';
+        $this->stock = '';
+        $this->imagen = '';
+        $this->categoria = '';
+        $this->estado = '';
+        $this->id_producto = '';
+    }
+
+    public function editar($id)
+    {
+        $producto = Producto::findOrFail($id);
+        $this->id_producto = $id;
+        $this->nombre = $producto->nombre;
+        $this->descripcion = $producto->descripcion;
+        $this->precio = $producto->precio;
+        $this->stock = $producto->stock;
+        $this->imagen = $producto->imagen;
+        $this->categoria = $producto->categoria;
+        $this->estado = $producto->estado;
+        $this->abrirModal();
+    }
+
+    public function borrar($id)
+    {
+        Producto::find($id)->delete();
+        session()->flash('mensaje', 'Producto eliminado correctamente');
+    }
+
+    public function guardar(){
+        Producto::updateOrCreate(['id' => $this->id_producto], [
+            'nombre' => $this->nombre,
+            'descripcion' => $this->descripcion,
+            'precio' => $this->precio,
+            'stock' => $this->stock,
+            'imagen' => $this->imagen,
+            'categoria' => $this->categoria,
+            'estado' => $this->estado,
+        ]);
+
+        session()->flash('mensaje', $this->id_producto ? 'Producto actualizado correctamente' : 'Producto creado correctamente');
+
+        $this->cerrarModal();
+        $this->limpiarCampos();
     }
 }
--- proyecto/resources/views/landing.blade.php
+++ proyecto/resources/views/landing.blade.php
@@ -99,91 +99,131 @@
     </div>
     <div class="flex justify-center flex-wrap gap-8 mb-5">
 
-      <div class="relative flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-blue-500 shadow-md">
-        <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
-          <img class="w-full h-full object-cover" src="storage/img/consolas2.jpg" alt="product image" />
-        </a>
-        <div class="mt-4 px-5 pb-5">
-          <a href="#">
-            <h5 class="text-xl tracking-tight text-white">Nintendo Switch</h5>
-          </a>
-          <div class="mt-2 mb-5 flex items-center justify-between">
-            <p>
-              <span class="text-3xl font-bold text-white">$300</span>
-            </p>
-          </div>
-          <a href="#" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium !text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            Add to cart</a
-          >
+        <div
+            class="relative flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-blue-500 shadow-md">
+            <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+                <img class="w-full h-full object-cover" src="storage/img/consolas2.jpg" alt="product image" />
+            </a>
+            <div class="mt-4 px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl tracking-tight text-white">Nintendo Switch</h5>
+                </a>
+                <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                        <span class="text-3xl font-bold text-white">$300</span>
+                    </p>
+                </div>
+                <a href="#"
+                    class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium !text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    Add to cart</a>
+            </div>
         </div>
-      </div>
-      <div class="relative  flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-blue-500 shadow-md">
-        <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
-          <img class="w-full h-full object-cover" src="storage/img/consolas2.jpg" alt="product image" />
-        </a>
-        <div class="mt-4 px-5 pb-5">
-          <a href="#">
-            <h5 class="text-xl tracking-tight text-white">Nintendo Switch</h5>
-          </a>
-          <div class="mt-2 mb-5 flex items-center justify-between">
-            <p>
-              <span class="text-3xl font-bold text-white">$300</span>
-            </p>
-          </div>
-          <a href="#" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium !text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            Add to cart</a
-          >
+        <div
+            class="relative  flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-blue-500 shadow-md">
+            <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+                <img class="w-full h-full object-cover" src="storage/img/consolas2.jpg" alt="product image" />
+            </a>
+            <div class="mt-4 px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl tracking-tight text-white">Nintendo Switch</h5>
+                </a>
+                <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                        <span class="text-3xl font-bold text-white">$300</span>
+                    </p>
+                </div>
+                <a href="#"
+                    class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium !text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    Add to cart</a>
+            </div>
         </div>
-      </div>
-      <div class="relative  flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-blue-500 shadow-md">
-        <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
-          <img class="w-full h-full object-cover" src="storage/img/consolas2.jpg" alt="product image" />
-        </a>
-        <div class="mt-4 px-5 pb-5">
-          <a href="#">
-            <h5 class="text-xl tracking-tight text-white">Nintendo Switch</h5>
-          </a>
-          <div class="mt-2 mb-5 flex items-center justify-between">
-            <p>
-              <span class="text-3xl font-bold text-white">$300</span>
-            </p>
-          </div>
-          <a href="#" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium !text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            Add to cart</a
-          >
+        <div
+            class="relative  flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-blue-500 shadow-md">
+            <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+                <img class="w-full h-full object-cover" src="storage/img/consolas2.jpg" alt="product image" />
+            </a>
+            <div class="mt-4 px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl tracking-tight text-white">Nintendo Switch</h5>
+                </a>
+                <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                        <span class="text-3xl font-bold text-white">$300</span>
+                    </p>
+                </div>
+                <a href="#"
+                    class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium !text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    Add to cart</a>
+            </div>
         </div>
-      </div>
-      <div class="relative  flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-blue-500 shadow-md">
-        <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
-          <img class="w-full h-full object-cover" src="storage/img/consolas2.jpg" alt="product image" />
-        </a>
-        <div class="mt-4 px-5 pb-5">
-          <a href="#">
-            <h5 class="text-xl tracking-tight text-white">Nintendo Switch</h5>
-          </a>
-          <div class="mt-2 mb-5 flex items-center justify-between">
-            <p>
-              <span class="text-3xl font-bold text-white">$300</span>
-            </p>
-          </div>
-          <a href="#" class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium !text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
-            <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-              <path stroke-linecap="round" stroke-linejoin="round" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-            </svg>
-            Add to cart</a
-          >
+        <div
+            class="relative  flex w-full max-w-64 flex-col overflow-hidden rounded-lg border border-gray-100 bg-blue-500 shadow-md">
+            <a class="relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl" href="#">
+                <img class="w-full h-full object-cover" src="storage/img/consolas2.jpg" alt="product image" />
+            </a>
+            <div class="mt-4 px-5 pb-5">
+                <a href="#">
+                    <h5 class="text-xl tracking-tight text-white">Nintendo Switch</h5>
+                </a>
+                <div class="mt-2 mb-5 flex items-center justify-between">
+                    <p>
+                        <span class="text-3xl font-bold text-white">$300</span>
+                    </p>
+                </div>
+                <a href="#"
+                    class="flex items-center justify-center rounded-md bg-slate-900 px-5 py-2.5 text-center text-sm font-medium !text-white hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="mr-2 h-6 w-6" fill="none" viewBox="0 0 24 24"
+                        stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                            d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
+                    </svg>
+                    Add to cart</a>
+            </div>
         </div>
-      </div>
-      
+
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mt-10 p-6 bg-blue-500 justify-center">
+        <div class="justify-center p-8">
+            <p class="text-center text-white text-xl md:text-2xl lg:text-3xl font-bold leading-tight uppercase">Compra
+                sin moverte</p>
+            <p class="text-white">
+                <br>Encuentra lo que necesitas, y coordina el pago y la entrega con el vendedor. Es fácil y rápido.
+                ¡Todos podemos hacerlo!
+            </p>
+        </div>
+        <div class="justify-center p-8">
+            <p class="text-center text-white text-xl md:text-2xl lg:text-3xl font-bold leading-tight uppercase">Recibe
+                tu producto </p><br>
+            <p class="text-white">
+                Acuerda la entrega de tu compra con el vendedor. Puedes recibirlo en tu casa, en la oficina o retirarlo.
+                ¡Tú decides qué prefieres!</p>
+        </div>
+
+        <div class="justify-center p-8">
+            <p class="text-center text-white text-xl md:text-2xl lg:text-3xl font-bold leading-tight uppercase">
+                TECNOIMPULSO</p><br>
+            <p class="text-white">
+                Descubre herramientas para gaming, trabajo y entretenimiento. Conecta con lo último en PCs, consolas y
+                móviles, y lleva tu experiencia al máximo nivel. ¡Tu pasión, nuestro soporte!</p>
+        </div>
+
+
     </div>
     <livewire:footer />
 </body>
